fd987c11c56bc8d288247cd70f79a2283eb8d182
Ez Belay <ezbelay@gmail.com>
2023-10-09 13:09:55 -0400

    updated landing page and intro to NN

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom';
 
 function LandingPage() {
     return (
-        <div>
+        <div style={{ margin: '0 200px', textAlign: 'center' }}>
             <h1><i>EzLLM</i>: An Interactive eLearning Web Application for Neural Networks</h1>
             <p>
                 Large language models (LLMs) have become the topic of discussion no matter what discipline you are in. I found it difficult to find a comprehensive yet simplified introduction to how these models work. <i><b>EzLLM</b></i> is an eLearning web application that will help people with no prior knowledge learn about neural networks and the transformer architecture.
             </p>
             <Link to="/newpage">
-                <button>Go to New Page</button>
+                <button>Go to Introduction to Neural Networks</button>
             </Link>
         </div>
+        
     );
 }
 
--- src/NewPage.jsx
+++ src/NewPage.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function NewPage() {
     return (
-        <div>This is a new page</div>
+        <div>
+            <h1>Introduction to Neural Networks</h1>
+            <p>A neural network is a computer system that's inspired by the way our brains work. Just like our brains have tiny cells called neurons that communicate with each other to process information, a neural network uses artificial neurons to process data. Each artificial neuron, like the one you see in the image, takes in information, processes it, and then passes it on to the next neuron. These neurons work together in layers, and they can learn and adapt over time, making neural networks really good at tasks like recognizing patterns in images, understanding spoken language, or making decisions based on data. So, think of a neural network as a digital brain that helps computers make sense of the world around them.</p>
+        </div>
     );
 }
 
