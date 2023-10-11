06ab5d17acb7dead27b832e8f6339384e14b5362
Ez Belay <ezbelay@gmail.com>
2023-10-10 20:45:25 -0400

    simple changes to clean up CSS and pages

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function LandingPage() {
     return (
-        <div style={{ margin: '0 200px', textAlign: 'center' }}>
+        <div className="centered-div">
             <h1><i>EzLLM</i>: An Interactive eLearning Web Application for Neural Networks</h1>
             <p>
                 Large language models (LLMs) have become the topic of discussion no matter what discipline you are in. I found it difficult to find a comprehensive yet simplified introduction to how these models work. <i><b>EzLLM</b></i> is an eLearning web application that will help people with no prior knowledge learn about neural networks and the transformer architecture.
